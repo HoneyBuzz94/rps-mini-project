0f91c32736a311324525c7fdb20b4ef0e5da79ba
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2023-11-08 21:28:39 -0700

    Made the code more efficient using an object

--- assets/script.js
+++ assets/script.js
@@ -3,70 +3,50 @@ var compChoice = "";
 var wins = 0;
 var losses = 0;
 var ties = 0;
+const weapons = {
+    f:{weakTo:"s",weakMsg:"Shield beats flail. You lose!",strongTo:"k",strongMsg:"Flail beats kindness. You win!"},
+    s:{weakTo:"k",weakMsg:"Kindness beats shield. You lose!",strongTo:"f", strongMsg:"Shield beats flail. You win!"},
+    k:{weakTo:"f",weakMsg:"Flail beats kindness. You lose!",strongTo:"s",strongMsg:"Kindness beats shield. You win!"}
+};
+var compOptions = ["f","s","k"];
 
-playBtn.addEventListener("click", gameStart)
+playBtn.addEventListener("click", runGame)
 
-function gameStart(){
-    var userInput = prompt("Rock, Paper, or Scissors? R = Rock, P = Paper, S = Scissors");
+function runGame(){
     compChooser();
 
-    if(userInput.toLowerCase() == compChoice.toLowerCase()){
-        ties += 1;
-        alert("It's a tie!")
-    } else if(userInput.toLowerCase() == "r"){
-        if(compChoice == "P"){
-            losses += 1;
-            alert("The computer picked Paper. You lose!");
-        } else if(compChoice == "S"){
+    try{
+        var userInput = prompt("Choose Flail, Shield, or Kindness! F = Flail, S = Shield, K = Kindness").toLowerCase();
+        if(userInput == compChoice){
+            alert("It's a tie!");
+            ties += 1;
+        } else if(weapons[userInput].strongTo == compChoice){
+            alert(weapons[userInput].strongMsg);
             wins += 1;
-            alert("The computer picked Scissors. You win!");
-        }
-    } else if(userInput.toLowerCase() == "p"){
-        if(compChoice == "S"){
+        } else if(weapons[userInput].weakTo == compChoice){
+            alert(weapons[userInput].weakMsg);
             losses += 1;
-            alert("The computer picked Scissors. You lose!");
-        } else if(compChoice == "R"){
-            wins += 1;
-            alert("The computer picked Rock. You win!");
         }
-    } else if(userInput.toLowerCase() == "s"){
-        if(compChoice == "P"){
-            wins += 1;
-            alert("The computer picked Paper. You win!");
-        } else if(compChoice == "R"){
-            losses += 1;
-            alert("The computer picked Rock. You lose!");
-        }
-    } else{
-        alert("Please enter R, P, or S to choose your weapon.")
+        playAgain();
+    }
+    catch{
+        alert("You must type in F, S, or K to select your weapon.");
         return;
     }
-    playAgain();
 }
 
 function playAgain(){
     var replay = confirm("Wins: "+wins+"    Losses: "+losses+"    Ties: "+ties+"\r\nWould you like to play again?");
     if(replay){
-        gameStart();
+        runGame();
     } else{
-        wins = 0;
-        losses = 0;
-        ties = 0;
+        wins = losses = ties = 0;
         return;
     }
 }
 
 function compChooser(){
-    var rndNum = Math.floor(Math.random()*3+1);
-    
-    if(rndNum == 1){
-        compChoice = "R";
-        return;
-    } else if(rndNum == 2){
-        compChoice = "P";
-        return;
-    } else if(rndNum == 3){
-        compChoice = "S";
-        return;
-    }
+    var rndNum = Math.floor(Math.random()*3);
+    compChoice = compOptions[rndNum];
+    return;
 }
